406e963e97f543ecd997700166d0a571d7c3eaed
Prathyusha0405 <nprathyushareddy04@gmail.com>
2025-03-25 08:17:46 +0000

    Delete Hackathon/Hackathon directory

--- Hackathon/Hackathon/src/main/java/com/Practice/Hackathon/Repository/ClaimDetailsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Practice.Hackathon.Repository;
-
-import com.Practice.Hackathon.model.ClaimDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClaimDetailsRepo extends JpaRepository<ClaimDetails, Long> {
-}
